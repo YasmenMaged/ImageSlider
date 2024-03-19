4999c6bb8fcb420906159c973bb730271ac08742
yasmen maged <gasmenmaged@gmail.com>
2024-03-19 16:01:24 +0200

    Add Images descriptions from an api

--- main.js
+++ main.js
@@ -1,10 +1,32 @@
+const apiUrl = "https://jsonplaceholder.typicode.com/posts";
 const previousButton = document.querySelector(".prev-btn");
 const nextButton = document.querySelector(".next-btn");
 const slider = document.querySelector(".slider");
 const slides = document.querySelectorAll(".slide");
+const slide = document.querySelector(".slide");
 const slideIcons = document.querySelectorAll(".slide-icon");
+const titles = document.querySelectorAll(".title");
+const descriptions = document.querySelectorAll(".description");
 const totalSlides = slides.length;
 let slideNumber = 0;
+
+ async function fetching(){
+ await fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      let titlesData = data.map(item => item.title);
+      const descriptionsData = data.map(item => item.body);
+      renderImageInfo(titlesData, descriptionsData);
+      showRandom(titlesData);
+    }).catch(error => console.error('Error fetching data:', error));
+  }
+fetching();
+function renderImageInfo(titlesData, descriptionsData) {
+  slides.forEach((slide, index) => {
+    titles[index].innerHTML = titlesData[index];
+    descriptions[index].innerHTML = descriptionsData[index];
+  });
+}
 
 function updateSlides() {
   slides.forEach((slide) => {
@@ -48,7 +70,18 @@ slider.addEventListener("dblclick", () => {
   clearInterval(playSlider);
 });
 
-function showRandom() {
+async function showRandom(titlesData) {
   let randomNumber = Math.floor(Math.random() * 6 + 1);
   document.getElementById("slide-img").src = "/images/" + randomNumber + ".jpg";
+  updateSlides();
+
+  console.log(titlesData);
+  
+      if(titlesData === true){
+        titles[randomNumber] = titlesData[randomNumber];
+      }
+      else{
+        console.log('error');
+        console.log(titlesData[randomNumber]);
+      }
 }
